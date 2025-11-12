7a12a99c4ce1fbd00075226d2330627cf50aace4
hassi999 <huzaifak076@gmail.com>
2025-11-13 01:46:27 +0500

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -94,7 +94,7 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/hassan-khan-668963384/" target="_blank">
                   <Linkedin />
                 </a>
                 <a href="https://x.com/rtzz_999" target="_blank">
